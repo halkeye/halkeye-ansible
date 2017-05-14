177f9e9048efdeb8a321b640662523f5606d2143
Gavin Mogan <git@gavinmogan.com>
2017-05-14 09:32:53 -0700

    blackpanther

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+which pip || sudo apt install python-pip python virtualenv libssl-dev -y
+test -e venv || virtualenv venv
+source venv/bin/activate
+which ansible || pip install ansible
 ansible-playbook -i "localhost," -c local $(hostname).yml  -K -b $@
--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
+which ansible || sudo apt install ansible -y
 ansible-galaxy install -p ./roles -r requirements.yml
