3981467b597d5557d9136a3d47bd624e60a11b0d
Gavin Mogan <git@gavinmogan.com>
2017-05-14 09:38:16 -0700

    manage git config, fixes #1

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-which pip || sudo apt install python-pip python virtualenv libssl-dev -y
-test -e venv || virtualenv venv
 source venv/bin/activate
-which ansible || pip install ansible
 ansible-playbook -i "localhost," -c local $(hostname).yml  -K -b $@
--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
-which ansible || sudo apt install ansible -y
+which pip || sudo apt install python-pip python virtualenv libssl-dev -y
+test -e venv || virtualenv venv
+source venv/bin/activate
+which ansible || pip install ansible
 ansible-galaxy install -p ./roles -r requirements.yml
