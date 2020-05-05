0036fbd094fad4b8e317f6e907ad8ad216db1c97
Gavin Mogan <gavin.mogan@risepeople.com>
2020-05-04 20:36:19 -0700

    migrate ansible to python3, which is defaulted in ubuntu 20.04

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-which pip || sudo apt install python-pip python-dev python libssl-dev build-essential -y
-which virtualenv || sudo pip install virtualenv
+which pip3 || sudo apt install python-pip python-dev python libssl-dev build-essential -y
+which virtualenv || sudo pip3 install virtualenv
 test -e venv || virtualenv venv
 source venv/bin/activate
 which ansible || pip install ansible
