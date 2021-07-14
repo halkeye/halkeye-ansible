f0cde07dd6dadd6142f6edb9562637a0626d4917
Gavin Mogan <gmogan@digitalocean.com>
2021-07-14 11:06:13 -0700

    upgrade to work stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euf -o pipefail
-which pip3 || sudo apt install python3-pip python3-dev python3 libssl-dev build-essential -y
+which pip3 || sudo apt install python3-pip python3-dev python3 libssl-dev build-essential git -y
 which virtualenv || sudo pip3 install virtualenv
 test -e venv || virtualenv venv
 source venv/bin/activate
