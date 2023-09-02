74cc667572d2c5bc5901bf68b4cb9eb88b3ffbb1
Gavin Mogan <git@gavinmogan.com>
2023-09-02 16:55:52 -0700

    setup forces zip/unzip to be installed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euf -o pipefail
-which pip3 || sudo apt install python3-pip python3-dev python3 libssl-dev build-essential git -y
-#which virtualenv || sudo pip3 install virtualenv
-#test -e venv || virtualenv venv
-#source venv/bin/activate
+which pip3 || sudo apt install -y python3-pip python3-dev python3 libssl-dev build-essential git
+which virtualenv || sudo pip3 install virtualenv
+which unzip || sudo apt install -y unzip
+which zip || sudo apt install -y zip
+test -e venv || virtualenv venv
+source venv/bin/activate
 which ansible || pip install ansible
