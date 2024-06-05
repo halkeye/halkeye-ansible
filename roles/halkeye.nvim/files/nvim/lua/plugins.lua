c4e6bcc6f3e3e92002bb06e13732a844fc258b79
Gavin Mogan <git@gavinmogan.com>
2024-06-04 23:22:35 -0700

    update nvim conform

--- roles/halkeye.nvim/files/nvim/lua/plugins.lua
+++ roles/halkeye.nvim/files/nvim/lua/plugins.lua
@@ -67,7 +67,44 @@ require("lazy").setup({
   "rcarriga/nvim-notify",
   {
     'stevearc/conform.nvim',
-    opts = {},
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        go = { "gofmt", "goimports-reviser", "golines" },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { "prettierd", "prettier", "eslint_d", "eslint" }, },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      -- Customize formatters
+      formatters = {
+        goimports = {
+          prepend_args = { "-rm-unused", "-company-prefixes", "do" }
+        },
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   },
   {
     "mfussenegger/nvim-lint",
