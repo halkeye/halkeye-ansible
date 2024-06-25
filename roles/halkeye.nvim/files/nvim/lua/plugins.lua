cd0957e04c73fffbd1389de646ca34da674517c7
Gavin Mogan <gmogan@digitalocean.com>
2024-06-24 17:00:50 -0700

    switch nvim color screen

--- roles/halkeye.nvim/files/nvim/lua/plugins.lua
+++ roles/halkeye.nvim/files/nvim/lua/plugins.lua
@@ -254,15 +254,24 @@ require("lazy").setup({
     },
   },
   {
-    "folke/tokyonight.nvim",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
-    opts = { style = "night" },
     init = function()
       vim.opt.background = "dark"
-      vim.cmd.colorscheme("tokyonight")
+      vim.cmd.colorscheme('gruvbox-material')
     end,
   },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = { style = "night" },
+  --   init = function()
+  --     vim.opt.background = "dark"
+  --     vim.cmd.colorscheme("tokyonight")
+  --   end,
+  -- },
   -- {
   --   "NLKNguyen/papercolor-theme",
   --   lazy = false,
