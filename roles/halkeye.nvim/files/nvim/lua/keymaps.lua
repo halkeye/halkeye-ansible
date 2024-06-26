9098791f4ab4bf9fe5a479dc3466c11f0d9d17c2
Gavin Mogan <git@gavinmogan.com>
2024-06-26 12:04:25 -0700

    add back some common shortcuts

--- roles/halkeye.nvim/files/nvim/lua/keymaps.lua
+++ roles/halkeye.nvim/files/nvim/lua/keymaps.lua
@@ -55,6 +55,12 @@ local bufopts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+
 -- show diagnostics in hover window
 vim.api.nvim_create_autocmd("CursorHold", {
   callback = function()
