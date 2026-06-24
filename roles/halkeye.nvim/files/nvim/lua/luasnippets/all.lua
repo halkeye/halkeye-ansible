f3dc5f768a5bd662833b1a845f1728d9021df43d
Gavin Mogan <git@gavinmogan.com>
2026-06-23 19:58:43 -0700

    snippets

--- roles/halkeye.nvim/files/nvim/lua/luasnippets/all.lua
+++ roles/halkeye.nvim/files/nvim/lua/luasnippets/all.lua
@@ -1,22 +1,26 @@
 local luasnip = require("luasnip")
 local s = luasnip.snippet
-local t = luasnip.text_node
 local f = luasnip.function_node
 
--- Function to generate 32 random hex chars
 local function generate_32_char_string()
-    local template = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    return string.gsub(template, "x", function()
-        return string.format("%x", math.random(0, 15))
-    end)
+	local template = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	return string.gsub(template, "x", function()
+		return string.format("%x", math.random(0, 15))
+	end)
 end
 
+local function generate_64_char_string()
+	local template = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	return string.gsub(template, "x", function()
+		return string.format("%x", math.random(0, 15))
+	end)
+end
 
 luasnip.add_snippets("all", {
-    s("rand32", {
-        f(generate_32_char_string, {}),
-    }),
-    s({trig="helloworld", snippetType="snippet", desc="A hello world snippet",wordTrig=true},
-        {t("Just a hint of what's to come!"),}
-    ),
+	s("rand32", {
+		f(generate_32_char_string, {}),
+	}),
+	s("rand64", {
+		f(generate_64_char_string, {}),
+	}),
 })
