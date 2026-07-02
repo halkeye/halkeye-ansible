1c87d35b92b3a1adb455ea8ed1ab4f4ac3965c44
Gavin Mogan <git@gavinmogan.com>
2026-07-02 09:34:11 -0700

    properly configure python stuff

--- roles/halkeye.nvim/files/nvim/lua/plugins.lua
+++ roles/halkeye.nvim/files/nvim/lua/plugins.lua
@@ -397,10 +397,20 @@ require("lazy").setup({
 				settings = {
 					pylsp = {
 						plugins = {
-							pycodestyle = {
-								ignore = { "W391" },
-								maxLineLength = 100,
-							},
+							-- formatter options
+							black = { enabled = true },
+							autopep8 = { enabled = false },
+							yapf = { enabled = false },
+							-- linter options
+							pylint = { enabled = true, executable = "pylint" },
+							pyflakes = { enabled = false },
+							pycodestyle = { enabled = false },
+							-- type checker
+							pylsp_mypy = { enabled = true },
+							-- auto-completion options
+							jedi_completion = { fuzzy = true },
+							-- import sorting
+							pyls_isort = { enabled = true },
 						},
 					},
 				},
