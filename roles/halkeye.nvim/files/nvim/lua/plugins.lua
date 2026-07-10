d770426194e765179f5d2ed8d7c638fb70e4bdb4
Gavin Mogan <gmogan@digitalocean.com>
2026-07-09 19:34:20 -0700

    vim: add in fzf again, planning to slowly switch from telescope

--- roles/halkeye.nvim/files/nvim/lua/plugins.lua
+++ roles/halkeye.nvim/files/nvim/lua/plugins.lua
@@ -902,4 +902,16 @@ require("lazy").setup({
 			vim.opt.foldlevelstart = 99
 		end,
 	},
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- or if using mini.icons/mini.nvim
+		-- dependencies = { "nvim-mini/mini.icons" },
+		---@module "fzf-lua"
+		---@type fzf-lua.Config|{}
+		---@diagnostic disable: missing-fields
+		opts = {},
+		---@diagnostic enable: missing-fields
+	},
 })
