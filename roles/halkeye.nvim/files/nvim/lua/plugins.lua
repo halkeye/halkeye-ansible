b3cedef5f54157e96ea0574fb110419afd9d0f8b
Gavin Mogan <gmogan@digitalocean.com>
2026-08-06 11:58:38 -0700

    delay which-key a bit so its not constantly flashing at me

--- roles/halkeye.nvim/files/nvim/lua/plugins.lua
+++ roles/halkeye.nvim/files/nvim/lua/plugins.lua
@@ -936,9 +936,9 @@ require("lazy").setup({
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+			delay = function(ctx)
+				return ctx.plugin and 0 or 500
+			end,
 		},
 		keys = {
 			{
